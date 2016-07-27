f6d61a419ce51a4438fdda1ba88f8835531bda32
Nick Silva <zenon.nicolas@gmail.com>
2016-07-27 15:46:36 -0700

    Add man plugin for colorized man page output

---
+++ plugins/available/man.plugin.bash
@@ -0,0 +1,16 @@
+cite about-plugin
+about-plugin 'colorize man page output'
+
+# get the full paths to binaries
+MAN_BIN=$(which man)
+
+function man () {
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+        LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+        LESS_TERMCAP_me=$'\E[0m' \
+        LESS_TERMCAP_se=$'\E[0m' \
+        LESS_TERMCAP_so=$'\E[38;5;246m' \
+        LESS_TERMCAP_ue=$'\E[0m' \
+        LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    "$MAN_BIN" "$@"
+}
